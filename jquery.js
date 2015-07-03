ecd023d01660a1deb52b51402022564b402955af
Don McLamb <mclamb.donald@gmail.com>
2015-07-03 15:50:10 -0400

    almost have magellan working, trying to modify the dom to get a solution. stopping efforts to move on to something else for now. making new branch

---
+++ jquery.js
@@ -0,0 +1,17 @@
+$('#contact').offset()
+Object {top: 2068.265625, left: 0}
+$('#contact').scrollTop()
+0
+$('body').scrollTop()
+1886
+
+
+
+.sub-nav dt.active a, .sub-nav dd.active a, .sub-nav li.active a {
+  border-radius: 3px;
+  background: #008CBA;
+  color: #fff;
+  cursor: default;
+  font-weight: normal;
+  padding: 0.1875rem 1rem;
+}
