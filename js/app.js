c74da48a2e5082caa645e1e4894c8720db1aac6b
Don McLamb <mclamb.donald@gmail.com>
2015-07-15 14:07:36 -0400

    trying media check not working yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+mediaCheck({
+  media: '(max-width: 901px)',
+  entry: function() {
+    $(document).ready(function(){
+      $('.slideshow').slick({
+        dots: true,
+        cssEase: 'linear',
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '40px',
+        arrows: true,
+        variableWidth: false,
+        // adaptiveHeight: true
+      });
+
+
+      console.log('starting ')
+
+    })
+    // exit: function(min-width 901px) {
+    //   console.log('exiting')
+    // }
+
+  })
+})
