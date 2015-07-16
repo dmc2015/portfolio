0537b8e0c5daf27419ce42aa7099cae88463b57e
Don McLamb <mclamb.donald@gmail.com>
2015-07-16 16:05:43 -0400

    merged and getting ready to deploy

--- index.php
+++ index.php
@@ -1,171 +1,329 @@
+
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation</title>
-    <link rel="stylesheet" href="stylesheets/app.css" />
-    <script src="bower_components/modernizr/modernizr.js"></script>
-  </head>
-  <body>
-    <div class="row">
-      <div class="large-12 columns">
-        <h1>Welcome to Foundation</h1>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="large-12 columns">
-        <div class="panel">
-          <h3>We&rsquo;re stoked you want to try Foundation! </h3>
-          <p>To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
-          <p>Once you've exhausted the fun in this document, you should check out:</p>
-          <div class="row">
-            <div class="large-4 medium-4 columns">
-              <p><a href="http://foundation.zurb.com/docs">Foundation Documentation</a><br />Everything you need to know about using the framework.</p>
-            </div>
-            <div class="large-4 medium-4 columns">
-              <p><a href="http://zurb.com/university/code-skills">Foundation Code Skills</a><br />These online courses offer you a chance to better understand how Foundation works and how you can master it to create awesome projects.</p>
-            </div>
-            <div class="large-4 medium-4 columns">
-              <p><a href="http://foundation.zurb.com/forum">Foundation Forum</a><br />Join the Foundation community to ask a question or show off your knowlege.</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-4 medium-4 medium-push-2 columns">
-              <p><a href="http://github.com/zurb/foundation">Foundation on Github</a><br />Latest code, issue reports, feature requests and more.</p>
-            </div>
-            <div class="large-4 medium-4 medium-pull-2 columns">
-              <p><a href="https://twitter.com/ZURBfoundation">@zurbfoundation</a><br />Ping us on Twitter if you have questions. When you build something with this we'd love to see it (and send you a totally boss sticker).</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+<head>
+	<title>CyberDesigns.io</title>
+	<meta charset="UTF-8"/>
+	<meta name = "viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <div class="row">
-      <div class="large-8 medium-8 columns">
-        <h5>Here&rsquo;s your basic grid:</h5>
-        <!-- Grid Example -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/normalize.css"/> -->
 
-        <div class="row">
-          <div class="large-12 columns">
-            <div class="callout panel">
-              <p><strong>This is a twelve column section in a row.</strong> Each of these includes a div.panel element so you can see where the columns are - it's not required at all for the grid.</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="large-6 medium-6 columns">
-            <div class="callout panel">
-              <p>Six columns</p>
-            </div>
-          </div>
-          <div class="large-6 medium-6 columns">
-            <div class="callout panel">
-              <p>Six columns</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout panel">
-              <p>Four columns</p>
-            </div>
-          </div>
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout panel">
-              <p>Four columns</p>
-            </div>
-          </div>
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout panel">
-              <p>Four columns</p>
-            </div>
-          </div>
-        </div>
+	<!-- GOOGLE API JS -->
+	<!-- <script src='https://www.google.com/recaptcha/api.js'></script> -->
 
-        <hr />
 
-        <h5>We bet you&rsquo;ll need a form somewhere:</h5>
-        <form>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-12.columns" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-4.columns" />
-            </div>
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-4.columns" />
-            </div>
-            <div class="large-4 medium-4 columns">
-              <div class="row collapse">
-                <label>Input Label</label>
-                <div class="small-9 columns">
-                  <input type="text" placeholder="small-9.columns" />
-                </div>
-                <div class="small-3 columns">
-                  <span class="postfix">.com</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Select Box</label>
-              <select>
-                <option value="husker">Husker</option>
-                <option value="starbuck">Starbuck</option>
-                <option value="hotdog">Hot Dog</option>
-                <option value="apollo">Apollo</option>
-              </select>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-6 medium-6 columns">
-              <label>Choose Your Favorite</label>
-              <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Radio 1</label>
-              <input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Radio 2</label>
-            </div>
-            <div class="large-6 medium-6 columns">
-              <label>Check these out</label>
-              <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox 1</label>
-              <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox 2</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Textarea Label</label>
-              <textarea placeholder="small-12.columns"></textarea>
-            </div>
-          </div>
-        </form>
-      </div>
+	<!-- ORIGINAL CSS -->
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.css">
+	<link rel="stylesheet" type="text/css" href="foundation-5.5.2/css/foundation.css"/>
+	<script src="foundation-5.5.2/js/vendor/modernizr.js"></script> -->
 
-      <div class="large-4 medium-4 columns">
-        <h5>Try one of these buttons:</h5>
-        <p><a href="#" class="small button">Simple Button</a><br/>
-        <a href="#" class="small radius button">Radius Button</a><br/>
-        <a href="#" class="small round button">Round Button</a><br/>
-        <a href="#" class="medium success button">Success Btn</a><br/>
-        <a href="#" class="medium alert button">Alert Btn</a><br/>
-        <a href="#" class="medium secondary button">Secondary Btn</a></p>
-        <div class="panel">
-          <h5>So many components, girl!</h5>
-          <p>A whole kitchen sink of goodies comes with Foundation. Check out the docs to see them all, along with details on making them your own.</p>
-          <a href="http://foundation.zurb.com/docs/" class="small button">Go to Foundation Docs</a>
-        </div>
-      </div>
-    </div>
+	<!-- THEME AND LAYOUT MAIN -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/theme.css"/> -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/layout.css"/> -->
 
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/foundation/js/foundation.min.js"></script>
-    <script src="js/app.js"></script>
-  </body>
+
+	<!-- // Add the slick-theme.css if you want default styling -->
+	<!-- <link rel="stylesheet" type="text/css" href="bower_components/slick.js/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="bower_components/slick.js/slick/slick-theme.css"/> -->
+
+	<!-- CUSTOM SLICK STYLING -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/slick.css"/> -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/contactform.css"/> -->
+
+	<!-- PAGE STYLING -->
+
+	<!-- CONTACT EMAIL -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/customcontact.css"/> -->
+
+
+	<!-- ABOUT  -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/about.css"/> -->
+
+
+	<!-- PICTURE -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/picturesection.css"/> -->
+
+
+
+	<!-- FONTS  -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,300|Flamenco:300,400|Advent+Pro:400,100|Orbitron:400,900,500|Merriweather:400,300' rel='stylesheet' type='text/css'>
+	<!-- <link rel="stylesheet" type="text/css" href="css/fonts.css"/> -->
+
+
+
+
+
+
+
+
+	<!--FOR SCSS -->
+	<link rel="stylesheet" href="stylesheets/app.css" />
+	<script src="bower_components/modernizr/modernizr.js"></script>
+
+
+</head>
+
+<body id="top">
+	<div class="contain-to-grid sticky">
+
+		<nav class="top-bar row" data-topbar>
+
+			<ul class="title-area">
+				<li class="name">
+					<h1><a href="#top">CyberDesigns.io <small>by  Don McLamb</small></a></h1>
+				</li>
+				<li class="toggle-topbar menu-icon">
+					<a href="#"><span>Menu</span></a>
+				</li>
+			</ul>
+
+
+			<section class ="top-bar-section">
+				<ul class="left small-4 main-nav">
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a  href="#portfolio">Portfolio</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+					<li>
+						<a href="docs/resume.pdf">Resume</a>
+					</li>
+				</ul>
+
+				<ul class="right small-4">
+					<li><a href="https://twitter.com/_D__o__N_"> <img class="social" src="images/twitter.png" alt="Twitter Account"/></a></li>
+					<li><a href="https://facebook.com/don.mclamb.16"> <img class="social" src="images/facebook.png" alt="Facebook Account"/></a></li>
+					<li><a href="https://linkedin.com/mclambdon"> <img class="social" src="images/linkedin.png" alt="LinkedIn Account"/></a></li>
+					<li><a href="https://github.com/dmc2015"><img class="social" src="images/merged-realgreen.png" alt="Github Account"/></a></li>
+				</li>
+			</ul>
+		</section>
+	</nav>
+</div>
+
+
+
+
+<main>
+	<section class=" image">
+		<img class="pro_photo" src="images/professional_photo.jpg" alt="Professional Photo"/>
+	</section>
+
+	<section id="about" class="subsection about panel">
+		<h1  class="aboutme text-center">About Me</h1>
+		<h1 class="name text-center">Don McLamb</h1>
+
+		<p class="show-on-shrink text-center aboutp">I am a full-stack web developer, graduate from General Assembly's web development immersive program with a Bachelors in Social Science and a Masters in Cybersecurity</p>
+
+		<p class="hide-on-shrink text-center aboutp">I am a full-stack web developer and a graduate from General Assembly's web development immersive program.</p>
+
+		<p class="hide-on-shrink text-center aboutp">Prior to that I received my B.S. in Psychology and Sociology from Towson University, worked in software support for three years and I received my Masters in CyberSecurity from UMUC in December of 2014.</p>
+
+		<p class="text-center aboutp">Cyberdesigns.io is a freelance company that I am starting to continue developing my skillset.</p>
+
+		<p class="text-center aboutp">Feel free to review my <a  href="#portfolio">Portfolio</a>, ask me questions and reach out to me via e-mail or social media.</p>
+		<p class="text-center aboutp">Thanks for stopping by!</p>
+	</section>
+</section>
+
+<section class="more-about row">
+	<h1 class="text-center section-heading">More About Me:</h1>
+
+	<div class= "fixing-slider">
+		<ul class="slideshow-about">
+
+			<li class="about-list">
+				<p class="about-slider text-center about-headings">Skills:</p>
+				<p class="about-slider text-left">* HTML5</p>
+				<p class="about-slider text-left">* CSS3 - Foundations, SASS, Compass</p>
+				<p class="about-slider text-left">* Ruby - Rails, Devise, Cancancan, Rspec, ActiveRecord</p>
+				<p class="about-slider text-left">* Javascript - jQuery, Backbone, Ajax, Jasmin, JSON</p>
+				<p class="about-slider text-left">* Other Technologies - Microsoft Server 2012, Postgresql, Git, AWS, Heroku, Bower, NPM</p>
+				<p class="about-slider text-left">* More Inside My<a href="docs/resume.pdf"> Resume</a></p>
+			</li>
+
+			<li class="text-left about-list">
+				<p class="about-slider text-center about-headings">What I’m excited about:</p>
+				<p class="about-slider text-left">* Using javascript to develop apps that are responsive on multiple device platforms</p>
+				<p class="about-slider text-left">* Next generation mobile devices</p>
+				<p class="about-slider text-left">* Penetration testing</p>
+				<p class="about-slider text-left">* Incorporating advanced security features into apps and developing security related apps and other security systems</p>
+			</li>
+
+			<li class="about-list">
+				<p class="about-slider text-center passions about-headings">Passions:</p>
+				<p class="about-slider text-left">* Learning new things</p>
+				<p class="about-slider text-left">* Problem solving</p>
+				<p class="about-slider text-left">* Tai Chi Chuan</p>
+				<p class="about-slider text-left">* Working out</p>
+			</li>
+
+			<li class="about-list">
+				<p class="about-slider text-center about-headings">Talents:</p>
+				<p class="about-slider text-left">* Resourcefulness</p>
+				<p class="about-slider text-left">* Persistence</p>
+				<p class="about-slider text-left">* Adaptability</p>
+			</li>
+
+			<li class="about-list">
+				<p class="about-slider text-center about-headings">What I am looking for:</p>
+				<p class="about-slider text-left">* Permanent positions as a front-end, back-end or full-stack developer</p>
+				<p class="about-slider text-left">* Cybersecurity positions</p>
+				<p class="about-slider text-left">* Freelance work</p>
+			</li>
+
+		</ul>
+
+	</div>
+</section>
+
+<!-- <h2>Skills:</h2>
+<ul id="skills">
+<li>HTML5</p></li>
+<li>CSS3 - Foundations</p></li>
+<li>Ruby - Rails, Devise, Cancancan, Rspec, ActiveRecord</p></li>
+<li>Javascript - jQuery, Backbone, Ajax, Jasmin, JSON</p></li>
+<li>Other Technologies - Microsoft Server 2012, Postgresql, Git, AWS, Heroku, </p></li>
+</ul> -->
+
+
+
+
+
+<section  id="portfolio" class="portfolio row">
+	<h1 class="text-center section-heading">Portfolio</h1>
+
+	<div text-center class="portfolio-slider-container">
+
+	<ul class="slideshow small-block-grid-3">
+		<li class="securitypulse project th">
+
+			<a  href="http://securitypulse.herokuapp.com"> <img src="images/securitypulse_1.png"/> </a>
+			<p class="text-center panel">A application designed encouraging and supporting a
+				secure web.</p>
+			</li>
+
+			<li class="policonnect project th">
+				<a class="" href="http://www.policonnect.org"> <img src="images/policonnect_1.png"/></a>
+				<p class="text-center panel">A group project to connect policymakers to experts in order to inform policymakers when making decisions.</p>
+			</li>
+
+			<!-- <li class="postpoll project th">
+			<a class="" href="http://localhost:3000"> <img src="images/postpoll.png"/></a>
+			</li> -->
+
+			<li class="writershub project th">
+				<a class="" href="https://the-writers-hub.herokuapp.com"> <img src="images/writershub_1.png"/></a>
+				<p class="text-center panel">A group project to assist writers in promoting their work and allow readers an opprotunity to read short stories.</p>
+			</li>
+
+			<li class="daytrackr project th">
+				<a class="" href="http://daytrackr.meteor.com"> <img src="images/daytrackr.png"/></a>
+				<p class="text-center panel">Prototype project for Meteor.js, a organizational app.</p>
+			</li>
+
+			<!-- <li class="todo project th">
+			<a class="" href="https://the-writers-hub.herokuapp.com"> <img src="images/"/></a>
+			<p class="text-center panel">A app mobile and web app created with Meteor.js that allows one to track their daily tasks.</p>
+			</li> -->
+
+		</ul>
+
+	</div>
+	</section>
+	<div id="contact" class="contact-form default-contact">
+
+		<h1 class="text-center contact-padding section-heading">Contact Me:</h1>
+
+		<p class="panel text-center email-contact">
+			<a href="mailto:mclamb.donald@gmail.com?subject=Hi I just saw your profile!">E-mail</a>
+		</p>
+		<!-- <php include("custom_form.php"); ?> -->
+		<!-- <div class="g-recaptcha" data-sitekey="6LcJdAgTAAAAAFNtfMQDBij8f1N6k8nCPk24ENv6"></div> -->
+
+	</div>
+
+
+</main>
+
+
+<footer>
+</footer>
+
+<!--FOR scss -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/foundation/js/foundation.min.js"></script>
+<script src="js/app.js"></script>
+
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="foundation-5.5.2/js/foundation.min.js"></script> -->
+
+<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.2.1/jquery-migrate.min.js"></script> -->
+
+<script>
+$(document).foundation();
+</script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.5/slick.min.js"></script>
+<!-- Initializing slick -->
+<script type="text/javascript">
+$(document).ready(function(){
+	$('.slideshow').slick({
+		dots: true,
+		cssEase: 'linear',
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		centerMode: true,
+		centerPadding: '40px',
+		arrows: true,
+		variableWidth: false,
+		// adaptiveHeight: true
+	});
+	$('.slideshow-about').slick({
+		dots: true,
+		cssEase: 'linear',
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: true,
+		centerPadding: '10px',
+		arrows: true,
+		// respondTo:'min',
+		variableWidth: false,
+		// adaptiveHeight: true
+	});
+});
+
+// infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+
+
+</script>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-64206324-1', 'auto');
+ga('send', 'pageview');
+
+</script>
+
+
+<!-- <script type="text/javascript" src="jquery_radiobutton.js"</script> -->
+
+
+
+</body>
 </html>
